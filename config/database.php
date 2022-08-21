7bb183bab524208ca57c204565db3354393ffb89
abubakar <2317abubakar@gmail.com>
2022-08-21 13:57:18 -0700

    changing

--- config/database.php
+++ config/database.php
@@ -58,6 +58,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => 'InnoDB',
+            // test for git
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
