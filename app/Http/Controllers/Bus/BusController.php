26c806dd2f5ca98db8ed92cd1dae84f626a06c7b
abubakar <abubakar@gmail.com>
2022-10-22 21:00:58 +0500

    pushing

--- app/Http/Controllers/Bus/BusController.php
+++ app/Http/Controllers/Bus/BusController.php
@@ -67,7 +67,6 @@ class BusController extends Controller
             'fare_class_id' => 'required|integer',
             'chassis_number' => 'required',
             'insurance_number' => 'required',
-            'no_of_seats' => 'required',
             'route_permit_number' => 'required',
         ];
 
@@ -76,7 +75,6 @@ class BusController extends Controller
             'fare_class_id.required' => 'Fare Class is Required!',
             'chassis_number.required' => 'Chassis Number is Required!',
             'insurance_number.required' => 'Insurance Number is Required!',
-            'no_of_seats.required' => 'Number Of Seats is Required!',
             'route_permit_number.required' => 'Route Permit is Required!',
         ];
         $this->validate($request, $rules, $customMessages);
@@ -84,7 +82,7 @@ class BusController extends Controller
             'bus_number' => $request->bus_number,
             'chassis_number' => $request->chassis_number,
             'insurance_number' => $request->insurance_number,
-            'no_of_seats' => $request->no_of_seats,
+            'no_of_seats' => $request->no_of_seats??0,
             'route_permit_number' => $request->route_permit_number,
             'fare_class_id' => $request->fare_class_id,
             'company_id' => $this->company_id,
