790273b31cefba43f99467595bb1cc37bd67ee3c
abubakar <abubakar@gmail.com>
2022-11-01 17:41:36 +0500

    env changes

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -21,8 +21,9 @@ import BookingPage from "./pages/booking/BookingPage.vue";
 import EmployeesPage from "./pages/hrm/employees/EmployeesPage.vue";
 import LeavePage from "./pages/hrm/leave/LeavePage.vue";
 
-const url = '/projects/kt/'
+// const url = '/projects/kt/'
 // const url = '/'
+const url = '/kt/'
 
 const routes = [
     {
