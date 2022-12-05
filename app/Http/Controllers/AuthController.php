10b7bf767208a93d2a24c06ae9558cb3f61ba463
Faizan ahmad <faizanmanni@gmail.com>
2022-12-05 11:38:07 +0500

    auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,15 +34,15 @@ class AuthController extends Controller
     {
 //        $ticket = Customer::with('tickets','tickets.schedule','tickets.customer','tickets.company','tickets.destination_city','tickets.departure_city','tickets.addedBy',)->where('id', 1)->first();
 
-        $ticket = Ticket::with('schedule', 'customer', 'company', 'destination_city', 'departure_city', 'addedBy')->where('customer_id', 1)->get();
-        $format = TicketsTemplate::where('company_id', 1)->where('status', 1)->first();
-            $pdf = PDF::loadView('pdf/pdf', ['data' => $ticket, 'data_terms'=> $format]);
-
-            $output = $pdf->output();
-
-        return new Response($output, 200, [
-            'Content-Type' => 'application/pdf',
-        ]);
+//        $ticket = Ticket::with('schedule', 'customer', 'company', 'destination_city', 'departure_city', 'addedBy')->where('customer_id', 1)->get();
+//        $format = TicketsTemplate::where('company_id', 1)->where('status', 1)->first();
+//            $pdf = PDF::loadView('pdf/pdf', ['data' => $ticket, 'data_terms'=> $format]);
+//
+//            $output = $pdf->output();
+//
+//        return new Response($output, 200, [
+//            'Content-Type' => 'application/pdf',
+//        ]);
         //elt pdf  test
 
 //        $elt =  TicketELT::with('addedBy', 'departure', 'destination', 'departure', 'updated_by', 'company', 'ticket', 'customer', 'schedule')->where('id', 1)->first();
