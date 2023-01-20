0e8da21ab630ae9123ec69ab8dbb256d91033a64
abdul rehman <malik.rehman7272@gmail.com>
2023-01-19 17:23:13 -0800

    report

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -70,4 +70,9 @@ class Ticket extends Model
     {
         return $this->hasOne(TicketELT::class, 'ticket_id', 'id');
     }
+
+    public function elt()
+    {
+        return $this->hasOne(TicketELT::class, 'ticket_id', 'id');
+    }
 }
