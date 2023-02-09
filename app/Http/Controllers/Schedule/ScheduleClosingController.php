e299b8b699c5d738671b696a4249bc16d5377b0e
Faizan Ahmad <faizanmanni@gmail.com>
2023-02-09 18:09:26 +0500

    live changes

--- app/Http/Controllers/Schedule/ScheduleClosingController.php
+++ app/Http/Controllers/Schedule/ScheduleClosingController.php
@@ -86,7 +86,7 @@ class ScheduleClosingController extends Controller
         if(count($bookingAvailable) == 0){
             return response()->json(["errors" => ["Tickets Error" => ["No Booking Found! \n\n Booked Any Single Seat First"]]], 422);
         }
-        
+
         $checkMergeRecord = TicketClosingMerge::where(["company_id" => Auth::user()->company_id, "bus_id" => $request->bus, "schedule_complete" => 0])->latest("id")->first();
         if ($checkMergeRecord) {
             TicketClosingMerge::where("id", $checkMergeRecord->id)->update([
