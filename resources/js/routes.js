28425f9112f2264d17888f3f8c4247489df6aba2
abdul rehman <malik.rehman7272@gmail.com>
2023-02-14 14:51:08 +0500

    changing

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -41,8 +41,8 @@ import TerminalDiscountPage from "./pages/terminal/TerminalDiscountPage.vue";
 import TerminalTimeDifferencePage from "./pages/terminal/TerminalTimeDifferencePage.vue";
 
 
-// const url = '/kt/'
-const url = '/'
+const url = '/kt/'
+// const url = '/'
 
 
 const routes = [
