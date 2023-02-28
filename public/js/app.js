38d529d330f891881711217b96e3faad04542e9a
Faizan Ahmad <faizanmanni@gmail.com>
2023-02-28 19:09:04 +0500

    changes live

--- public/js/app.js
+++ public/js/app.js
@@ -72248,10 +72248,9 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+ // const url = '/kt/'
 
-var url = '/kt/'; //
-// const url = '/'
-
+var url = '/';
 var routes = [{
   path: url + "",
   component: _pages_users_Users_vue__WEBPACK_IMPORTED_MODULE_1__["default"],
