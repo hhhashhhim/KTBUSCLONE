c4ccf21ceb4333077883820f8e07bbd2b7b7e5fa
abdul rehman <malik.rehman7272@gmail.com>
2023-03-15 16:33:08 +0500

    point addition

--- app/Http/Controllers/Booking/BookingController.php
+++ app/Http/Controllers/Booking/BookingController.php
@@ -102,7 +102,6 @@ class BookingController extends Controller
                     $finalAmountDiscount = (int)($amountInFlat - $request->totalFare);
                 }
             }
-            dd($amountInPercent , $request->totalFare, $amountInPercent,$card->percentage_discount,  $cardAssign->starting_points);
             $schedule = Schedule::where('id', $request->schedule)->where('company_id', Auth::user()->company_id)->select('id', 'fare_class_id', 'route_id', 'bus_class_id')->with('bus_class:id,seat_map', 'route:id,name', 'route.fares:id,route_id,departure_city_id,destination_city_id')->first();
             $departure_city_id = $schedule->route->fares->first()->departure_city_id;
             $destination_city_id = $schedule->route->fares->last()->destination_city_id;
@@ -204,6 +203,21 @@ class BookingController extends Controller
                 }
                 $allTicket[] = $ticket->id;
             }
+        }
+        // loyalty card point addition
+        $checkCard = CardAssign::where(['cnic' => plainContactAndCnic($request->customerCNIC), 'company_id' => Auth::user()->company_id])->with("cardCategory")->first();
+        if($checkCard)
+        {
+            if($checkCard->cardCategory->point_type == "flatPoints")
+            {
+                $addPoint = $request->totalAmount/$checkCard->cardCategory->point_flat;
+            }
+            else
+            {
+                $distance = FareTable::where(['from_city_id' => $request->departureCity,'to_city_id' => $request->destinationCity, 'company_id' => Auth::user()->company_id])->first()->distance_in_km;
+                $addPoint = $distance/$checkCard->cardCategory->point_distance;
+            }
+            $checkCard->increment("starting_points",$addPoint);
         }
         return [
             'ids' => implode('-', $allTicket),
