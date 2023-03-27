9535428c6331b79b758b0e9d454bbc1f3c86eb4e
Faizan Ahmad <faizanmanni@gmail.com>
2023-03-27 11:42:20 +0500

    Reports panel

--- resources/js/layouts/company/SideBar.vue
+++ resources/js/layouts/company/SideBar.vue
@@ -63,50 +63,50 @@
                     </ul>
                 </li>
                 <!-- Reporting panel -->
-<!--                <li class="dropdown" v-if="checkPermission('reports')">-->
-<!--                    <a href="#" class="menu-toggle nav-link has-dropdown"><i class="fas fa-ticket-alt"></i>-->
-<!--                        <span>-->
-<!--                            Reports-->
-<!--                        </span>-->
-<!--                    </a>-->
-<!--                    <ul class="dropdown-menu">-->
-<!--                        <li class="dropdown" v-if="checkForSubmenu('confirm-cancel')">-->
-<!--                            <router-link class="nav-link text-capitalize"  to="">-->
-<!--                                <i class="fas fa-city"></i> Confirmed Canceled-->
-<!--                            </router-link>-->
-<!--                        </li>-->
-<!--                        <li class="dropdown" v-if="checkForSubmenu('sales')">-->
-<!--                            <router-link class="nav-link text-capitalize"  to="">-->
-<!--                                <i class="fa fa-landmark"></i> Sales Report-->
-<!--                            </router-link>-->
-<!--                        </li>-->
-<!--                        <li class="dropdown" v-if="checkForSubmenu('close-trip')">-->
-<!--                            <router-link class="nav-link text-capitalize"  to="">-->
-<!--                                <i class="fas fa-clock"></i> Closed Trip Report-->
-<!--                            </router-link>-->
-<!--                        </li>-->
-<!--                        <li class="dropdown" v-if="checkForSubmenu('commission')">-->
-<!--                            <router-link class="nav-link text-capitalize"  to="">-->
-<!--                                <i class="fas fa-table"></i> Commission Reports-->
-<!--                            </router-link>-->
-<!--                        </li>-->
-<!--                        <li class="dropdown" v-if="checkForSubmenu('complaints')">-->
-<!--                            <router-link class="nav-link text-capitalize"  to="">-->
-<!--                                <i class="fas fa-table"></i> Complaints Reports-->
-<!--                            </router-link>-->
-<!--                        </li>-->
-<!--                        <li class="dropdown" v-if="checkForSubmenu('over-issue')">-->
-<!--                            <router-link class="nav-link text-capitalize"  to="">-->
-<!--                                <i class="fa fa-bus"></i> Over-Issue Reports-->
-<!--                            </router-link>-->
-<!--                        </li>-->
-<!--                        <li class="dropdown" v-if="checkForSubmenu('expenses')">-->
-<!--                            <router-link class="nav-link text-capitalize"  to="">-->
-<!--                                <i class="fa fa-bus"></i> Expenses Report-->
-<!--                            </router-link>-->
-<!--                        </li>-->
-<!--                    </ul>-->
-<!--                </li>-->
+                <li class="dropdown" v-if="checkPermission('reports')">
+                    <a href="#" class="menu-toggle nav-link has-dropdown"><i class="fas fa-ticket-alt"></i>
+                        <span>
+                            Reports
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown" v-if="checkForSubmenu('confirm-cancel')">
+                            <router-link class="nav-link text-capitalize"  to="">
+                                <i class="fas fa-city"></i> Confirmed Canceled
+                            </router-link>
+                        </li>
+                        <li class="dropdown" v-if="checkForSubmenu('sales')">
+                            <router-link class="nav-link text-capitalize"  to="">
+                                <i class="fa fa-landmark"></i> Sales Report
+                            </router-link>
+                        </li>
+                        <li class="dropdown" v-if="checkForSubmenu('close-trip')">
+                            <router-link class="nav-link text-capitalize"  to="">
+                                <i class="fas fa-clock"></i> Closed Trip Report
+                            </router-link>
+                        </li>
+                        <li class="dropdown" v-if="checkForSubmenu('commission')">
+                            <router-link class="nav-link text-capitalize"  to="">
+                                <i class="fas fa-table"></i> Commission Reports
+                            </router-link>
+                        </li>
+                        <li class="dropdown" v-if="checkForSubmenu('complaints')">
+                            <router-link class="nav-link text-capitalize"  to="">
+                                <i class="fas fa-table"></i> Complaints Reports
+                            </router-link>
+                        </li>
+                        <li class="dropdown" v-if="checkForSubmenu('over-issue')">
+                            <router-link class="nav-link text-capitalize"  to="">
+                                <i class="fa fa-bus"></i> Over-Issue Reports
+                            </router-link>
+                        </li>
+                        <li class="dropdown" v-if="checkForSubmenu('expenses')">
+                            <router-link class="nav-link text-capitalize"  to="">
+                                <i class="fa fa-bus"></i> Expenses Report
+                            </router-link>
+                        </li>
+                    </ul>
+                </li>
                 <!-- Buses panel -->
                 <li class="dropdown" v-if="checkPermission('buses')">
                     <a href="#" class="menu-toggle nav-link has-dropdown"><i class="fas fa-ticket-alt"></i>
