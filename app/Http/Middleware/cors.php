405c761b4e1ef86c51ab84823592f9a2381febfd
Faizan Ahmad <faizanmanni@gmail.com>
2023-05-16 18:45:56 +0500

    live changes

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -16,9 +16,10 @@ class cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin',"*")
-            ->header('Access-Control-Allow-Methods',"PUT,POST,DELETE,GET,OPTIONS")
-            ->header('Access-Control-Allow-Headers',"Accept,Authorization,Content-Type");
+        return $next($request);
+//        return $next($request)
+//            ->header('Access-Control-Allow-Origin',"*")
+//            ->header('Access-Control-Allow-Methods',"PUT,POST,DELETE,GET,OPTIONS")
+//            ->header('Access-Control-Allow-Headers',"Accept,Authorization,Content-Type");
     }
 }
