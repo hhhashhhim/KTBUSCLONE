fc274533858bde4a71a3c2f3603ddd6fac3a4fed
Faizan Ahmad <faizanmanni@gmail.com>
2023-05-18 01:25:28 +0500

    chnags

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,12 +10,14 @@ class CorsMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse) $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if ( !str_contains(url(''), 'localhost')) {
+            return $next($request);
+        }
     }
 }
--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
-    'allowed_origins' => ['https://example.com'],
+    'allowed_origins' => ['*'],
     'allowed_headers' => ['Content-Type', 'X-Requested-With'],
     'exposed_headers' => [],
     'max_age' => 0,
