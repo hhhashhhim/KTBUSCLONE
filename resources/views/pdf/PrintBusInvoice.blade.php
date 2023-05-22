55d1ce533f06031d386d3e7ee2bb92f766075a1e
abdul rehman <malik.rehman7272@gmail.com>
2023-05-22 17:11:28 +0500

    kt commission

--- resources/views/pdf/PrintBusInvoice.blade.php
+++ resources/views/pdf/PrintBusInvoice.blade.php
@@ -172,7 +172,7 @@
                 @php
                     $totalElt += $destination->sum("elt_price")
                 @endphp
-                <td>{{ ((($destination->sum("seat_fare") + $destination->sum("elt_price")) - ($destination->sum("discount"))) - $commission) - $totalAdjustCommission }}</td>
+                <td>{{ ((($destination->sum("seat_fare") + $destination->sum("elt_price")) - ($destination->sum("discount"))) - $commission) - $adjustCommission }}</td>
             </tr>
         @endforeach
     @endforeach
