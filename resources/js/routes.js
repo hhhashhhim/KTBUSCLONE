1a0c83c5b48d32477ed9878f803129f01cfab5f4
abdul rehman <malik.rehman7272@gmail.com>
2023-06-05 16:29:06 +0500

    change

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -50,7 +50,6 @@ import AdvanceSaleReportsPage from "./pages/Sales/AdvanceSaleReportsPage.vue";
 import ConfirmCancellationPage from "./pages/Cancel/ConfirmCancelationPage.vue";
 import CounterExpensesPage from "./pages/expense/CounterExpensesPage.vue";
 
-
 // const url = '/kt/'
 const url = '/'
 
