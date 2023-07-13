01a3af8201799aa045c9c64ee331e887201bcf6d
abdul rehman <malik.rehman7272@gmail.com>
2023-07-13 12:38:03 +0500

    change

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -11,7 +11,7 @@ class AuthApiController extends Controller
 {
     function login(Request $request)
     {
-        $user= User::where('email', $request->email)->first();
+        $user= User::where(['email'=> $request->email,"hide"=>0])->first(["id","name","email","contact"]);
         // print_r($data);
             if (!$user || !Hash::check($request->password, $user->password)) {
                 return response([
