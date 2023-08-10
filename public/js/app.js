1fbe1528539a23f428411cd214d50980ca9b061b
abdul rehman <malik.rehman7272@gmail.com>
2023-08-10 16:55:34 +0500

    change

--- public/js/app.js
+++ public/js/app.js
@@ -84330,9 +84330,9 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+ // const url = '/kt/'
 
-var url = '/kt/'; // const url = '/'
-
+var url = '/';
 var routes = [{
   path: url + "",
   component: _pages_users_Users_vue__WEBPACK_IMPORTED_MODULE_1__["default"],
