ff7464d04137e37797b321ca392b6a386573286a
abdul rehman <malik.rehman7272@gmail.com>
2023-08-17 16:53:30 +0500

    change

--- app/Http/Controllers/Booking/BookingController.php
+++ app/Http/Controllers/Booking/BookingController.php
@@ -71,6 +71,7 @@ class BookingController extends Controller
     public function store(Request $request)
     {
         try {
+            $lock = Cache::lock('foo', 10)->block(5, function () {
             DB::beginTransaction();
             if ($request->terminalId == 0 && is_null(Auth::user()->terminal_id)) {
                 return response()->json(["errors" => ["Booking Error" => ["If You Are Company Admin Please Assign Terminal To Your Account  For Booking the Ticket, If You Are Employee Of Company Please Contact Your Administrator Or IT Team! "]]], 422);
@@ -251,6 +252,8 @@ class BookingController extends Controller
                 'ticket' => Ticket::where('company_id', Auth::user()->company_id)->whereIn('id', $allTicket)->get(),
                 'authTerminalId' => Auth::user()->terminal_id,
             ];
+        });
+        return $lock;
         } catch (\Exception $e) {
             DB::rollBack();
             Log::error('Database transaction error: ' . $e->getMessage());
