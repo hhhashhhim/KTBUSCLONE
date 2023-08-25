45cdc3856dadf745545f2055642c597711270f79
abdul rehman <malik.rehman7272@gmail.com>
2023-08-25 14:31:05 +0500

    changing

--- resources/views/pdf/pdf.blade.php
+++ resources/views/pdf/pdf.blade.php
@@ -72,9 +72,9 @@
             window.print();
         });
 
-        setTimeout(function(){
-            window.close() ;
-        }, 1000); //Time before execution
+        // setTimeout(function(){
+        //     window.close() ;
+        // }, 1000); //Time before execution
     </script>
     <title>Print Ticket</title>
 </head>
