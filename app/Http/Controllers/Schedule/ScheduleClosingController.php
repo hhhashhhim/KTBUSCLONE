d60e92fbdb0bf2f8c6c60c91881e1b73f3738967
abdul rehman <malik.rehman7272@gmail.com>
2023-08-25 16:32:48 +0500

    change

--- app/Http/Controllers/Schedule/ScheduleClosingController.php
+++ app/Http/Controllers/Schedule/ScheduleClosingController.php
@@ -46,7 +46,7 @@ class ScheduleClosingController extends Controller
 
     public function merges()
     {
-        $merges = TicketClosingMerge::where(['company_id' => Auth::user()->company_id, 'schedule_complete' => 1,"id"=>198])
+        $merges = TicketClosingMerge::where(['company_id' => Auth::user()->company_id, 'schedule_complete' => 1])
             ->with("bus:id,bus_number")
             ->with("closing:id,ticket_merge_id,schedule_id", "closing.schedule:id,name")
             ->with("tickets.elt:id,ticket_id,elt_price","tickets.schedule:id,route_id")
