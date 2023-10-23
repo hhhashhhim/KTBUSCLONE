58a377e01fae12efd6e37b49dd873a3404b1481e
abdul rehman <malik.rehman7272@gmail.com>
2023-10-23 14:57:14 +0500

    changing

--- app/Http/Controllers/Api/TicketingApiController.php
+++ app/Http/Controllers/Api/TicketingApiController.php
@@ -147,7 +147,7 @@ class TicketingApiController extends Controller
                     $single->total_fare = $original_fare;
                     $single->final_fare = $discounted_fare;
                     $single->departure_date_time = date("Y-m-d H:i:s", strtotime($single->departure_date . ' ' . $single->departure_time));
-                    $single->departure_time = date("h:i:s A", strtotime($single->departure_time));
+                    $single->departure_time = date("h:i A", strtotime($single->departure_time));
                     
                 });
                 if(checkPermissionButtons("time-lock"))
