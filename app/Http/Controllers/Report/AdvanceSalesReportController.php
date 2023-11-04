d1d77c2210fb62255d6772d62393162333a4c630
abdul rehman <malik.rehman7272@gmail.com>
2023-11-04 16:30:23 +0500

    changing

--- app/Http/Controllers/Report/AdvanceSalesReportController.php
+++ app/Http/Controllers/Report/AdvanceSalesReportController.php
@@ -56,7 +56,7 @@ class AdvanceSalesReportController extends Controller
             ->get();
 
         $tickets->transform(function ($single) {
-            $single->schedule_date_time = date('Y-m-d H:i:s', strtotime($single->schedule_date . ' ' . $single->schedule->time));
+            $single->schedule_date_time = date('Y-m-d H:i:s', strtotime($single->schedule_date . ' ' . $single->schedule_time));
             return $single;
         });
 
