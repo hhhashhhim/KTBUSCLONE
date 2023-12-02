0c75b8327cadfec6b1e16f92f0ad2d76c8cdc5b6
abdul rehman <malik.rehman7272@gmail.com>
2023-12-02 14:29:32 +0500

    test

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -26,7 +26,6 @@ class AuthApiController extends Controller
 
         $user= User::where(['email'=> $request->email,"hide"=>0])->first(["id","name","email","contact","password"]);
         // print_r($data);
-        return Hash::check($request->password, $user->password);
             if (!$user || !Hash::check($request->password, $user->password)) {
                 return response([
                     'message' => ['These credentials do not match our records.']
