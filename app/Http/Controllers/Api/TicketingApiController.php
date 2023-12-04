2f92be19990f74d2c3b85d05e4c356eeb8d6774a
abdul rehman <malik.rehman7272@gmail.com>
2023-12-04 13:07:57 +0500

    test

--- app/Http/Controllers/Api/TicketingApiController.php
+++ app/Http/Controllers/Api/TicketingApiController.php
@@ -40,7 +40,7 @@ class TicketingApiController extends Controller
     
     public function availableSchedules(Request $request)
     {
-        try {return 'helo';
+        try {
             
                 $validator = Validator::make($request->all(), [
                     'departure_city_id' => 'required',
