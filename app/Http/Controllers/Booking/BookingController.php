ffb7b648a8f95bb810ac4ab47939b42daf680809
abdul rehman <malik.rehman7272@gmail.com>
2023-12-27 14:36:10 +0500

    reschedule error

--- app/Http/Controllers/Booking/BookingController.php
+++ app/Http/Controllers/Booking/BookingController.php
@@ -360,7 +360,7 @@ class BookingController extends Controller
                     'customer_id' => $item['dataCustomer'],
                     'schedule_id' => $item['newDepartureTime'],
                     'route_id' => $schedule->route_id,
-                    'remarks' => $item['reason'],
+                    'remarks' => $item['reason']??"",
                     'gender' => $ticket['gender'],
                     'type' => $item['rescheduleType'],
                     'reschedule_type' => $item['overIssueReschedule'],
