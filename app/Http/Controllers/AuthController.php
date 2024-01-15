12941d960554edf5381a7ace4e5294bf7fded7fe
abdul rehman <malik.rehman7272@gmail.com>
2024-01-15 13:03:28 +0500

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,7 +65,7 @@ class AuthController extends Controller
     }
 
     public function logout(Request $request)
-    {
+    {return 'helo';
         ActivityLog::create([
             "activity_by" => Auth::user()->id,
             "message" => Auth::user()->name." | logout",
