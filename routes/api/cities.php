469d022f73a9e7fcb3b4dd976d38ec099b4c400b
abdul rehman <malik.rehman7272@gmail.com>
2024-01-15 15:52:23 +0500

    test

--- routes/api/cities.php
+++ routes/api/cities.php
@@ -11,10 +11,3 @@ Route::group(['prefix' => 'web/v1/cities','middleware' => ['auth:sanctum']], fun
     Route::post('hide', [CityController::class, 'hideCity']);
     Route::post('/terminals', [CityController::class, 'cityTerminals']);
 });
-Route::group(['prefix' => 'web/v1/citi'], function () {
-    Route::post('/', [CityController::class, 'index']);
-    Route::post('store', [CityController::class, 'store']);
-    Route::post('update', [CityController::class, 'update']);
-    Route::post('hide', [CityController::class, 'hideCity']);
-    Route::post('/terminals', [CityController::class, 'cityTerminals']);
-});
