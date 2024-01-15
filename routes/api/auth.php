0ff36b482f172a7445f7749ca2aa9f5255c25303
abdul rehman <malik.rehman7272@gmail.com>
2024-01-15 13:07:49 +0500

    test

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -7,4 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::post("/web/v1/login", [AuthController::class, 'login']);
 Route::post("/web/v1/double-check", [AuthController::class, 'doubleCheck']);
 Route::post("/web/v1/password/update", [AuthController::class, 'updatePassword']);
-Route::get("/web/v1/logout", [AuthController::class, 'logout'])->middleware([CustomMiddleware::class]);
+Route::post("/web/v1/logout", [AuthController::class, 'logout']);
