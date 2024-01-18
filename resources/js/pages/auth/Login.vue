120bc92f2af6f555849f658cfcfa8212507ba5dc
abdul rehman <malik.rehman7272@gmail.com>
2024-01-18 16:23:11 +0500

    check

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -147,8 +147,7 @@ export default {
                 return this.errorsArray("Password is Required", "Password");
             
             const res = await this.callApi("post", "login", this.data);
-            
-            // token created and login
+          
             if (res.status == 201) {
                 
                 if (res.data.user) {
