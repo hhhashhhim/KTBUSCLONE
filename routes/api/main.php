0fe0a98a1eccb4e6797937a95895821fd27d32ec
abdul rehman <malik.rehman7272@gmail.com>
2024-03-20 12:57:56 +0500

    bar button

--- routes/api/main.php
+++ routes/api/main.php
@@ -38,9 +38,5 @@ Route::group(['middleware' => 'auth:sanctum'], function(){
       Route::post('/status/check',[BookingApiController::class,'checkTicketsStatus']);
    });
    
-   Route::group(['prefix'=>'v1/ticketing'],function(){
-      Route::post('/schedules/available',[TicketingApiController::class,'availableSchedules']);
-      Route::post('/schedule/preview',[TicketingApiController::class,'previewSchedule']);
-      Route::post('/new',[TicketingApiController::class,'bookSeat']);
-   });
+  
 });
