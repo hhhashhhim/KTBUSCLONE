144b2f452b48e8c6e5bd978b520f184cfe5a0c63
abdul rehman <malik.rehman7272@gmail.com>
2024-03-29 16:03:43 +0500

    change

--- app/Http/Controllers/Schedule/ScheduleController.php
+++ app/Http/Controllers/Schedule/ScheduleController.php
@@ -229,7 +229,7 @@ class ScheduleController extends Controller
                 }
                 ActivityLog::create([
                     "activity_by" => Auth::user()->id,
-                    "message" => Auth::user()->name." | updated schedule time from $request->start_date to $request->end_date time ($request->time) | $request->schedule_id",
+                    "message" => Auth::user()->name." | updated schedule time from $request->start_date to $request->end_date time ($request->time) | $request->schedule_id $request->start_date $request->end_date",
                     "requested_host" => $request->ip(),
                     "company_id" => Auth::user()->company_id
                 ]);
