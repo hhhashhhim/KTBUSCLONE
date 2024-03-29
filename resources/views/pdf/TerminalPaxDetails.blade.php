8fd2d0a82e8d90318ffd0b88c8682000823b49e8
abdul rehman <malik.rehman7272@gmail.com>
2024-03-29 14:25:31 +0500

    change

--- resources/views/pdf/TerminalPaxDetails.blade.php
+++ resources/views/pdf/TerminalPaxDetails.blade.php
@@ -145,7 +145,7 @@
                 <td>{{ ucfirst($item->destination_city->name) }}</td>
                 <td>{{ $item->ticketElt == null ? 0 : $item->ticketElt->elt_price }}</td>
                 <td>{{ $item->seat_fare }}</td>
-                <td>{{ $item->discount }}</td>
+                <td>{{ intVal($item->discount) }}</td>
                 <td>{{ ucfirst($item->terminal->name) }}</td>
                 <td>{{ ucfirst($item->updated_name->name??'N/A') }}</td>
 
