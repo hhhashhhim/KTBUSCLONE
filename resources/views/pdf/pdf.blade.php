82b27e48ba43972710a17e3cbf02cb9e34fe5bcc
abdul rehman <malik.rehman7272@gmail.com>
2024-04-01 15:01:41 +0500

    voucher

--- resources/views/pdf/pdf.blade.php
+++ resources/views/pdf/pdf.blade.php
@@ -136,7 +136,7 @@
 
             <div class="clear-both">
                 <p class="my-5 font-weight-bold float-left">Booking Date :</p>
-                <p class="my-5 float-right">{{ date('d/m/Y H:i A', strtotime($data['tickets'][$key]['booked_time'])) }}</p>
+                <p class="my-5 float-right">{{ date('d/m/Y h:i A', strtotime($data['tickets'][$key]['booked_time'])) }}</p>
             </div>
 
             <div class="clear-both">
