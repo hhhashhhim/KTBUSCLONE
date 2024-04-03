e911a523bd6752284666270594027cd3ec3a586a
abdul rehman <malik.rehman7272@gmail.com>
2024-04-03 15:02:08 +0500

    bus invoice

--- app/Http/Controllers/Booking/BookingController.php
+++ app/Http/Controllers/Booking/BookingController.php
@@ -1339,7 +1339,6 @@ class BookingController extends Controller
     public
     function terminalInvoice(Request $request)
     {
-        return $request;
         $scheduleDetail = ScheduleDetail::where([
             'company_id' => Auth::user()->company_id,
             'schedule_id' => $request->schedule_id,
