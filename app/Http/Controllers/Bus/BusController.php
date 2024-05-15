9df9af5f9189a83ea989647c41b211df938f28cb
abdul rehman <malik.rehman7272@gmail.com>
2024-05-15 13:48:47 +0500

    rectify

--- app/Http/Controllers/Bus/BusController.php
+++ app/Http/Controllers/Bus/BusController.php
@@ -31,36 +31,6 @@ class BusController extends Controller
 
     public function storeBus(Request $request)
     {
-        $detailGroup = ScheduleDetail::where(["company_id"=>Auth::user()->company_id,"schedule_id"=>349])->where("schedule_date",'>', "2024-05-14")->get()->groupBy("schedule_date");
-                
-        foreach($detailGroup as $detail)
-        {
-            foreach($detail as $key => $single)
-            {
-                $updatedTime = date("Y-m-d H:i:s",strtotime(($single->departure_date.' '.$single->departure_time)));
-                if($key==0)
-                {
-                    $schedule_date = date("Y-m-d",strtotime($updatedTime));
-                }
-                $single->update([
-                    "departure_date" => date("Y-m-d",strtotime($updatedTime)),
-                    "departure_time" => date("H:i:s",strtotime($updatedTime)),
-                    "schedule_date" => $schedule_date,
-                ]);
-                Ticket::where(["company_id"=>Auth::user()->company_id,"schedule_id"=>349])->where([
-                    "date"=>$single->departure_date,
-                    "departure_city_id"=>$single->departure_id,
-                    "destination_city_id"=>$single->destination_id,
-                    "schedule_time"=>$single->departure_time,
-                ])->update([
-                    "schedule_date"=>$schedule_date
-                ]);
-                
-            }
-        }
-
-
-        return 'h';
         // $data = ScheduleDetail::where(["company_id"=>Auth::user()->company_id,"schedule_date"=>"2024-02-25","schedule_id"=>330])->get();
         
         // foreach($data as $single)
