4fda4710077b079a47bd25b0d4aaff7b7d406179
abdul rehman <malik.rehman7272@gmail.com>
2024-06-25 14:08:09 +0500

    change

--- app/Http/Controllers/Api/BookingApiController.php
+++ app/Http/Controllers/Api/BookingApiController.php
@@ -829,7 +829,8 @@ class BookingApiController extends Controller
                     ]);
                     $allTicket = [];
                     foreach ($request->selected_seats as $i => $seat) {
-                        $checkDiscount =  checkDiscountAmount($detail,$terminalId,$request->selected_seats_class[$i]);
+                        // $checkDiscount =  checkDiscountAmount($detail,$terminalId,$request->selected_seats_class[$i]);
+                        $checkDiscount =  0;
                         $ticket = Ticket::create([
                             'company_id' => $companyId,
                             'departure_city_id' => $request->departure_city_id,
