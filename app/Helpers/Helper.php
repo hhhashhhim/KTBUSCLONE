adf3f724b7486b86089ff985bfa419659b27bf0f
abdul rehman <malik.rehman7272@gmail.com>
2024-07-11 16:47:11 +0500

    change

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -418,7 +418,7 @@ if (!function_exists('ticketConfirmedMessage')) {
             4 => 'Hamza_4-Device4',
             5 => 'Hamza_4-Device-5'
         ];
-        $randomNumber = rand(4, 5);
+        $randomNumber = rand(1, 5);
         $session = $names[$randomNumber];
 
 
