616a45b956bc7042ee97ad89df1baaedaf84dc61
abdul rehman <malik.rehman7272@gmail.com>
2024-07-18 17:11:33 +0500

    change

--- app/Http/Controllers/TerminalController.php
+++ app/Http/Controllers/TerminalController.php
@@ -352,6 +352,7 @@ class TerminalController extends Controller
 
     public function timeStore(Request $request)
     {
+        return $request;
         if(!checkPermissionButtons("edit-terminal"))
         {
             return response()->json(["Error" => ['You are not authorized to access this url']], 403);
