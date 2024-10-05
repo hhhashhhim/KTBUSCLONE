75861ea50c007dd614fa28baa021e2d43a48d0e8
abdul rehman <malik.rehman7272@gmail.com>
2024-10-05 17:27:45 +0500

    seat duplication

--- app/Http/Controllers/Api/BookingApiController.php
+++ app/Http/Controllers/Api/BookingApiController.php
@@ -768,6 +768,11 @@ class BookingApiController extends Controller
                     {
                         $ticketDepIndex = array_search($tkt->departure_city_id,$allFaresOfRoute);
                         $ticketDesIndex = array_search($tkt->destination_city_id,$allFaresOfRoute);
+                        if(($ticketDepIndex >= $scheduleDepIndex && $ticketDepIndex < $scheduleDesIndex)
+                         || ($ticketDesIndex > $scheduleDepIndex && $ticketDesIndex <= $scheduleDesIndex))
+                        {
+                            return response()->json(["errors" => ["Error" => ["One seat of your combination already booked"]]], 422);
+                        }
                         if(($scheduleDepIndex >= $ticketDepIndex && $scheduleDepIndex < $ticketDesIndex)
                          || ($scheduleDesIndex > $ticketDepIndex && $scheduleDesIndex <= $ticketDesIndex))
                         {
--- app/Http/Controllers/Booking/BookingController.php
+++ app/Http/Controllers/Booking/BookingController.php
@@ -224,7 +224,6 @@ class BookingController extends Controller
                 ->get();
             }
 
-            
             foreach($checkAlreadyBooked as $tkt)
             {
                 //Check if Ticket is Booked for RWP to MORO Target is to book MORO to Karachi
@@ -240,6 +239,11 @@ class BookingController extends Controller
                 *   Condition 2 )($ticketDesIndex > $scheduleDepIndex && $ticketDesIndex <= $scheduleDesIndex)
                 *   Output 1 > 1 > 1 && 1 <= 2 Result False
                 */
+                if( ($ticketDepIndex >= $scheduleDepIndex && $ticketDepIndex < $scheduleDesIndex) // Will Check Partial Seat 
+                    || ($ticketDesIndex > $scheduleDepIndex && $ticketDesIndex <= $scheduleDesIndex))
+                {
+                    return response()->json(["errors" => ["Error" => ["One seat of your combination is already booked"]]], 422);
+                }
                 if( ($scheduleDepIndex >= $ticketDepIndex && $scheduleDepIndex < $ticketDesIndex) // Will Check Partial Seat 
                     || ($scheduleDesIndex > $ticketDepIndex && $scheduleDesIndex <= $ticketDesIndex))
                 {
