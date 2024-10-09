1d3dfcc64901115a213bcce0ae90b450754d211d
abdul rehman <malik.rehman7272@gmail.com>
2024-10-09 12:22:38 +0500

    change

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -68,8 +68,8 @@ import ReschedulePage from "./pages/Cancel/ReschedulePage.vue";
 import CounterExpensesPage from "./pages/expense/CounterExpensesPage.vue";
 import OfficeExpensesPage from "./pages/expense/OfficeExpensesPage.vue";
 
-const url = '/kt-dev/'
-// const url = '/'
+// const url = '/kt-dev/'
+const url = '/'
 
 
 const routes = [
