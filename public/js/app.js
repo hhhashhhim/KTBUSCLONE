71d6d05958dd718b4bad74479f46ab6e9c6a1f61
abdul rehman <malik.rehman7272@gmail.com>
2024-11-11 14:50:03 +0500

    detail to other terminal

--- public/js/app.js
+++ public/js/app.js
@@ -36071,7 +36071,11 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
                       _this26.addForm.advanceSeatClass.push(data["class"]);
 
-                      if (_this26.auth_terminal.other_terminal_passenger_detail == 1 || parseInteger(data.terminal) == _this26.auth_terminal.id) {
+                      console.log(data.terminal);
+                      console.log(_this26.auth_terminal.id);
+                      console.log(data.terminal == _this26.auth_terminal.id);
+
+                      if (_this26.auth_terminal.other_terminal_passenger_detail == 1 || data.terminal == _this26.auth_terminal.id) {
                         _this26.addForm.customerCNIC = data.customer_cnic != 0 ? data.customer_cnic : "";
                         _this26.addForm.customerName = data.customer_name;
                         _this26.addForm.contact = data.customer_phone;
--- resources/js/pages/booking/BookingPage.vue
+++ resources/js/pages/booking/BookingPage.vue
@@ -3174,7 +3174,10 @@ export default {
                         this.addForm.alreadyBookedId.push(data.id);
                         this.addForm.reservedFare.push(data.fare);
                         this.addForm.advanceSeatClass.push(data.class);
-                        if(this.auth_terminal.other_terminal_passenger_detail == 1 || parseInteger(data.terminal) == this.auth_terminal.id)
+                        console.log(data.terminal);
+                        console.log(this.auth_terminal.id);
+                        console.log(data.terminal == this.auth_terminal.id);
+                        if(this.auth_terminal.other_terminal_passenger_detail == 1 || data.terminal == this.auth_terminal.id)
                         {
                             this.addForm.customerCNIC = data.customer_cnic != 0 ? data.customer_cnic : "";
                             this.addForm.customerName = data.customer_name;
