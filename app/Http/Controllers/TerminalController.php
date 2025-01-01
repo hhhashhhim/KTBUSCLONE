18b0b95f031f359addb582f175f28d9e13919991
abdul rehman <malik.rehman7272@gmail.com>
2025-01-01 16:25:09 +0500

    change

--- app/Http/Controllers/TerminalController.php
+++ app/Http/Controllers/TerminalController.php
@@ -473,7 +473,7 @@ class TerminalController extends Controller
             }
 
             $single->refund = $refundValue;
-            $single->schedule_date_time = date('Y-m-d H:i:s', strtotime($single->schedule_date . ' ' . $single->schedule_time_exact));
+            $single->schedule_date_time = date('Y-m-d H:i:s', strtotime($single->date . ' ' . $single->schedule_time_exact));
             return $single;
         });
 
