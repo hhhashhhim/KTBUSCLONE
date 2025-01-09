d5c75e4b2b9989696cdb2531c83920b233d66521
abdul rehman <malik.rehman7272@gmail.com>
2025-01-09 13:11:21 +0500

    change

--- app/Http/Controllers/Schedule/ScheduleClosingController.php
+++ app/Http/Controllers/Schedule/ScheduleClosingController.php
@@ -507,10 +507,10 @@ class ScheduleClosingController extends Controller
         try {
             DB::beginTransaction();
             
-            $depTime = ScheduleDetail::where(["schedule_id" => $request->schedule,
+            return $depTime = ScheduleDetail::where(["schedule_id" => $request->schedule,
                 "departure_id" => $request->departureCity,
                 "destination_id" => $request->destinationCity,
-                "departure_date" => $request->date,
+                "schedule_date" => $request->date,
                 'departure_time' =>  date("H:i:s",strtotime($request->departure_time)),
                 "company_id" => Auth::user()->company_id
             ])->first();
