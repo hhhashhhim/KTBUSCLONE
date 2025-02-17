b3179b6a7650f2206bcfef7e851fc4a355834c20
unknown <malik.rehman7272@gmail.com>
2025-02-17 12:58:32 +0500

    change

--- app/Http/Controllers/Api/BookingApiController.php
+++ app/Http/Controllers/Api/BookingApiController.php
@@ -179,11 +179,11 @@ class BookingApiController extends Controller
                         unset($data[$key]);
                     }
                     // this is subroute visibility to check that this terminal if allow to fetch of specific subroute schedule
-                    $visibility =TerminalVisibility::where(["departure_city_id"=>$single->departure_id,"destination_city_id"=>$single->destination_id,"route_id"=>$single->schedule->route_id])->first();
-                    if(isset($visibility) && $visibility->online_visibilty == 1)
-                    {
-                        unset($data[$key]);
-                    }
+                    // $visibility =TerminalVisibility::where(["departure_city_id"=>$single->departure_id,"destination_city_id"=>$single->destination_id,"route_id"=>$single->schedule->route_id])->first();
+                    // if(isset($visibility) && $visibility->online_visibilty == 1)
+                    // {
+                    //     unset($data[$key]);
+                    // }
 
 
 
@@ -295,7 +295,7 @@ class BookingApiController extends Controller
 
                 if($companyId == 2)
                 {
-                    return 'h';
+                    // return 'h';
                     return $data;
                 }
 
