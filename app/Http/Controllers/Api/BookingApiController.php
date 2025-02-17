d7f0e1721bb9204a47a008f6afa8e7cbf8a1b3ad
unknown <malik.rehman7272@gmail.com>
2025-02-17 13:11:14 +0500

    change

--- app/Http/Controllers/Api/BookingApiController.php
+++ app/Http/Controllers/Api/BookingApiController.php
@@ -292,6 +292,13 @@ class BookingApiController extends Controller
 
                 });
 
+                if($companyId == 2)
+                {
+                    return 'h';
+                    return $data;
+                }
+
+
                 $data = $data->where("departure_date_time",'>',date("Y-m-d H:i:s",strtotime(date("Y-m-d H:i:s")) + 5400))->sortBy("departure_date_time");
                 $arrayData = json_decode($data, true);
                 $data = collect(array_values($arrayData));
