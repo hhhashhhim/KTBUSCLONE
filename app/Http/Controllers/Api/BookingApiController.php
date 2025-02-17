3fc7fc11afae127960bac4ae3b8d4386a5ef067b
unknown <malik.rehman7272@gmail.com>
2025-02-17 13:05:27 +0500

    change

--- app/Http/Controllers/Api/BookingApiController.php
+++ app/Http/Controllers/Api/BookingApiController.php
@@ -295,7 +295,7 @@ class BookingApiController extends Controller
 
                 if($companyId == 2)
                 {
-                    return 'h';
+                    // return 'h';
                     return $data;
                 }
 
