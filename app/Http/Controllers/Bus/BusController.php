b68ee97bfbb4c08345f27b14b6c43f6217b78181
unknown <malik.rehman7272@gmail.com>
2025-03-10 12:53:19 +0500

    chage

--- app/Http/Controllers/Bus/BusController.php
+++ app/Http/Controllers/Bus/BusController.php
@@ -22,7 +22,7 @@ class BusController extends Controller
 {
     public function index()
     {
-        return $pending_merges = Ticket::where("ticket_closing_id",'=',null)->where("bus_id",'!=',null)->distinct("schedule_id")->count();
+        return $pending_merges = Ticket::where("ticket_merge_id",'=',null)->where("bus_id",'!=',null)->distinct("schedule_id")->count();
         if(!checkForSubmenu("buses"))
         {
             return response()->json(["Error" => ['You are not authorized to access this url']], 403);
