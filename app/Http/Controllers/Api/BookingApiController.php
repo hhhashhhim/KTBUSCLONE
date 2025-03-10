f80d0085e0b798bfd29c904685176965f5d5777c
unknown <malik.rehman7272@gmail.com>
2025-03-10 12:12:33 +0500

    chage

--- app/Http/Controllers/Api/BookingApiController.php
+++ app/Http/Controllers/Api/BookingApiController.php
@@ -237,7 +237,8 @@ class BookingApiController extends Controller
                         })
                         ->first();
                         $scheduleSurcharge = Surcharge::where('id', $single->schedule->surcharge_id)->where('is_active', 1)->first();
-                        $terminalDiscount = TerminalDiscount::where(["terminal_id" => $terminalId ?? 0, "route_id" => $single->schedule->route_id])->first();
+                        $terminalDiscount = TerminalDiscount::where(["terminal_id" => $terminalId ?? 0, "route_id" => $single->schedule->route_id])->where('start_date', '<=', date("Y-m-d"))
+                        ->where('end_date', '>=', date("Y-m-d"))->first();
 
                         $editFare = $fare;
                         if ($scheduleDiscount) {
@@ -401,7 +402,8 @@ class BookingApiController extends Controller
                     ], 422);
                 }
         //        //Apply terminal discount
-                $terminalDiscount = TerminalDiscount::where(["terminal_id" => $terminalId ?? 0, "route_id" => $schedule->route_id])->first();
+                $terminalDiscount = TerminalDiscount::where(["terminal_id" => $terminalId ?? 0, "route_id" => $schedule->route_id])->where('start_date', '<=', date("Y-m-d"))
+                ->where('end_date', '>=', date("Y-m-d"))->first();
                 $seatChoices =  $schedule->route->online_seat_choices ? explode(",",$schedule->route->online_seat_choices) : null;
                 // Looping Through the seat of the bus
                 $seatMap = $scheduleDetail->bus_class->seat_map;
