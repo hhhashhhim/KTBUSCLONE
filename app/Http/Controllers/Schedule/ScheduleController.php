deb713f7ed7aa2acf428763379abe00abdd59c90
unknown <malik.rehman7272@gmail.com>
2025-05-23 12:58:59 +0500

    change

--- app/Http/Controllers/Schedule/ScheduleController.php
+++ app/Http/Controllers/Schedule/ScheduleController.php
@@ -33,7 +33,7 @@ class ScheduleController extends Controller
         {
             return response()->json(["Error" => ['You are not authorized to access this url']], 403);
         }
-        return ScheduleDetail::limit(100)->get();
+        return ScheduleDetail::get();
         $schedules = ScheduleDetail::
         with('schedule.route','bus_class')
         ->whereHas('schedule', function($q)use($request){
