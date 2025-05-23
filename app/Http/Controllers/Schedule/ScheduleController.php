079bfec4cb008f37cf5ea65fafbe8a51b3b09a2f
unknown <malik.rehman7272@gmail.com>
2025-05-23 15:43:37 +0500

    change

--- app/Http/Controllers/Schedule/ScheduleController.php
+++ app/Http/Controllers/Schedule/ScheduleController.php
@@ -55,7 +55,7 @@ class ScheduleController extends Controller
             })
             ->where(['company_id' => Auth::user()->company_id])
             ->orderby("id","ASC")
-            ->limit(200000)
+            ->limit(250000)
             ->get()->unique("schedule_id");
 
             return $schedules;
