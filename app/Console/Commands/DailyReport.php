43686691cae54202ac4214806710eb031acb3175
ShkAdnan <adnanasad@outlook.com>
2025-11-12 13:27:46 +0500

    changes

--- app/Console/Commands/DailyReport.php
+++ app/Console/Commands/DailyReport.php
@@ -85,20 +85,20 @@ class DailyReport extends Command
             ->select('customer_id', 'date')
             ->distinct()
             ->get();
+            
+            $cancel_ids = $ticketData->where("date", $today)->where("type", "canceled")->pluck('id');
+            $pending_merges = Ticket::where("ticket_closing_id",'=',null)->distinct("schedule_id")->count();
+        // $pending_merges = TicketClosing::where('company_id', $company_id)
+        //     ->where('hide', 0)
+        //     ->where('commission_route', 0)
+        //     ->whereNotNull('ticket_merge_id')
+        //     ->get()
+        //     ->groupBy('ticket_merge_id')
+        //     ->filter(function ($group) {
+        //         return $group->count() === 1;
+        //     })
+        //     ->count();
 
-        $cancel_ids = $ticketData->where("date", $today)->where("type", "canceled")->pluck('id');
-        $pending_merges = TicketClosing::where('company_id', $company_id)
-            ->where('hide', 0)
-            ->where('commission_route', 0)
-            ->whereNotNull('ticket_merge_id')
-            ->get()
-            ->groupBy('ticket_merge_id')
-            ->filter(function ($group) {
-                return $group->count() === 1;
-            })
-            ->count();
-
-        // $pending_merges = Ticket::where("ticket_closing_id",'=',null)->distinct("schedule_id")->count();
 
         $today_confirm = $ticketData->where("date", $today)->where("type", "booked")->count();
         $today_reserve = $ticketData->where("date", $today)->where("type", "advance booking")->count();
