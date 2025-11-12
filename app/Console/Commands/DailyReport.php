762190fc31e8eb0df33c6afafe4e8dc022ac8624
ShkAdnan <adnanasad@outlook.com>
2025-11-12 13:35:16 +0500

    changes

--- app/Console/Commands/DailyReport.php
+++ app/Console/Commands/DailyReport.php
@@ -90,6 +90,8 @@ class DailyReport extends Command
         $cancel_ids = $ticketData->where("date", $today)->where("type", "canceled")->pluck('id');
        $pending_merges = DB::table('ticket_closings')
     ->select('ticket_merge_id', DB::raw('COUNT(*) as total'))
+    ->where('hide', 0)
+    ->where('commission_route', 0)
     ->whereNotNull('ticket_merge_id')
     ->groupBy('ticket_merge_id')
     ->having('total', '=', 1)
