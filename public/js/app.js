0f644b6fb9dc8a815c90492bebdefae21b69f8ed
ShkAdnan <adnanasad@outlook.com>
2025-11-22 23:05:50 +0500

    chnages

--- public/js/app.js
+++ public/js/app.js
@@ -134762,9 +134762,9 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+ // const url = "/kt-dev/";
 
-var url = "/kt-dev/"; // const url = '/' 
-
+var url = '/';
 var routes = [{
   path: url + "",
   component: _pages_users_Users_vue__WEBPACK_IMPORTED_MODULE_1__["default"],
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -88,8 +88,8 @@ import JazzCashRefund from "./pages/booking/JazzCashRefund.vue";
 import DiscountCardAssignPage from "./pages/discountType/DiscountCardAssignPage.vue";
 import DiscountType from "./pages/discountType/DiscountCardTypePage.vue";
 
-const url = "/kt-dev/";
-// const url = '/' 
+// const url = "/kt-dev/";
+const url = '/' 
 
 const routes = [
     {
