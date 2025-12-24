cd0509d802826207efab4f9a0ec5b1c8687947b0
ShkAdnan <adnanasad@outlook.com>
2025-12-24 12:25:32 +0500

    changes

--- app/Http/Controllers/Booking/AllBookingController.php
+++ app/Http/Controllers/Booking/AllBookingController.php
@@ -208,7 +208,6 @@ class AllBookingController extends Controller
         }
 
         $data['pp_SecureHash'] = hash_hmac('sha256', $hashString, $integritySalt);
-        return $data;
         // Log::info('🎯 JazzCash Refund Payload', $data);
 
         // 🌐 Send refund request
