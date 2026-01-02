c662ea46fd4ae0b49c280ea7649e75bd6317515a
ShkAdnan <adnanasad@outlook.com>
2026-01-02 20:54:24 +0500

    new changes

--- public/js/app.js
+++ public/js/app.js
@@ -139179,8 +139179,8 @@ var store = (0,vuex__WEBPACK_IMPORTED_MODULE_0__.createStore)({
       },
       user: JSON.parse(localStorage.getItem("user")),
       token: localStorage.getItem("token"),
-      main_url: "https://demo.kainattravels.net/",
-      api_url: "https://demoapi.kainattravels.net/",
+      main_url: "https://portal.kainattravels.net/",
+      api_url: "https://api.kainattravels.net/",
       permissions: JSON.parse(localStorage.getItem("user")) && JSON.parse(localStorage.getItem("user")).role ? JSON.parse(localStorage.getItem("user")).role.permissions : [],
       companyModules: false
     };
