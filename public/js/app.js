3003a72713de9506f0b84139c2157a25e3382640
ShkAdnan <adnanasad@outlook.com>
2026-01-24 16:38:38 +0500

    merges

--- public/js/app.js
+++ public/js/app.js
@@ -139245,6 +139245,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _pages_booking_JazzCashRefund_vue__WEBPACK_IMPORTED_MODULE_85__ = __webpack_require__(/*! ./pages/booking/JazzCashRefund.vue */ "./resources/js/pages/booking/JazzCashRefund.vue");
 /* harmony import */ var _pages_discountType_DiscountCardAssignPage_vue__WEBPACK_IMPORTED_MODULE_86__ = __webpack_require__(/*! ./pages/discountType/DiscountCardAssignPage.vue */ "./resources/js/pages/discountType/DiscountCardAssignPage.vue");
 /* harmony import */ var _pages_discountType_DiscountCardTypePage_vue__WEBPACK_IMPORTED_MODULE_87__ = __webpack_require__(/*! ./pages/discountType/DiscountCardTypePage.vue */ "./resources/js/pages/discountType/DiscountCardTypePage.vue");
+/* provided dependency */ var process = __webpack_require__(/*! process/browser.js */ "./node_modules/process/browser.js");
 
 
 
@@ -139335,7 +139336,7 @@ __webpack_require__.r(__webpack_exports__);
 
  // const url = "/kt-dev/";
 
-var url = '/';
+var url = process.env(MIX_API_URL_ROUTE);
 var routes = [{
   path: url + "",
   component: _pages_users_Users_vue__WEBPACK_IMPORTED_MODULE_1__["default"],
