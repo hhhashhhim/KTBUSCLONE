01549bdce3bc8341f83ee5d0ddc2eac0e61a88f6
ShkAdnan <adnanasad@outlook.com>
2026-02-04 16:47:49 +0500

    merges

--- app/Http/Controllers/Booking/BookingController.php
+++ app/Http/Controllers/Booking/BookingController.php
@@ -1876,11 +1876,11 @@ if (!empty($request->usageDiscount) && !empty($request->discount_otp_valid) && $
             'id' => Auth::user()->terminal_id,
         ])->first()->name : "Not Assigned Terminal";
 
-        $mainData = Ticket::withTrashed()->where([
+       return $mainData = Ticket::withTrashed()->where([
             'tickets.company_id' => Auth::user()->company_id,
             'tickets.schedule_id' => $request->schedule_id,
             'tickets.schedule_date' => $uniqueDate,
-        ])
+        ])->where('type', '!=', 'reschedule')->where('type', '!=', 'canceled')->where('type', '!=', 'advance booking')
             ->with("terminal:id,name", "destination_city:id,name")
             ->with(["commission" => function ($q) use ($route) {
                 return $q->where("route_id", $route->id);
