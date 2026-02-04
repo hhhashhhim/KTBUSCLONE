cc508e89e4caa4b082693fb364f304fe9448008b
ShkAdnan <adnanasad@outlook.com>
2026-02-04 16:44:17 +0500

    merges

--- app/Http/Controllers/Booking/BookingController.php
+++ app/Http/Controllers/Booking/BookingController.php
@@ -1840,8 +1840,7 @@ if (!empty($request->usageDiscount) && !empty($request->discount_otp_valid) && $
         return view('pdf/TerminalPaxDetails', ['data' => $passengerData, 'terminal' => $terminal, "format" => $format]);
     }
 
-    public
-    function busInvoice(Request $request)
+    public function busInvoice(Request $request)
     {
         if (!checkPermissionButtons("bus-invoice")) {
             return response()->json(["Error" => ['You are not authorized to access this url']], 403);
@@ -1881,7 +1880,7 @@ if (!empty($request->usageDiscount) && !empty($request->discount_otp_valid) && $
             'tickets.company_id' => Auth::user()->company_id,
             'tickets.schedule_id' => $request->schedule_id,
             'tickets.schedule_date' => $uniqueDate,
-        ])->where('type', '!=', 'reschedule')->where('type', '!=', 'canceled')->where('type', '!=', 'advance booking')
+        ])->where('type', '!=', 'canceled')
             ->with("terminal:id,name", "destination_city:id,name")
             ->with(["commission" => function ($q) use ($route) {
                 return $q->where("route_id", $route->id);
