d183f9064b55bb821e52108e4aefc98c6f404888
ShkAdnan <adnanasad@outlook.com>
2026-02-12 19:58:14 +0500

    merges

--- app/Http/Controllers/Api/BookingApiController.php
+++ app/Http/Controllers/Api/BookingApiController.php
@@ -364,7 +364,7 @@ class BookingApiController extends Controller
             ])->first();
 
             if ($terminalId == 14){
-                return $companyId;
+                return $scheduleDetail;
             }
             // Getting Already Booked Tickets
             $tickets = Ticket::with('departure_city', 'destination_city', 'schedule', 'customer', 'company', 'addedBy')
