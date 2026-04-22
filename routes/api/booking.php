9c77c9522ba2f01b936d53e5ae7a6f9e713073ad
Farhan-Ali1786 <farhan12345ali12346@gmail.com>
2026-04-22 16:05:20 +0500

    changes

--- routes/api/booking.php
+++ routes/api/booking.php
@@ -57,7 +57,6 @@ Route::group(['prefix' => 'web/v1/booking','middleware' => ['auth:sanctum']], fu
     Route::post('/fetch/over/issue/seat', [BookingController::class, 'fetchOverIssueSeat']);
     Route::post('/revert/over/issue/seat', [BookingController::class, 'revertOverIssueSeat']);
     Route::post('/whatsapp/message', [BookingController::class, 'whatsappMessage']);
-    Route::post('/whatsapp/message', [BookingController::class, 'onlineWhatsappMessage']);
     Route::post('/whatsapp/cancel/message', [BookingController::class, 'whatsappCancelMessage']);
     Route::post('/whatsapp/bus/send-message', [BookingController::class, 'sendMessageToBus']);
 
