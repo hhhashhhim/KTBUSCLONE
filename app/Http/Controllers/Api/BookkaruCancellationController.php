f5e985482e442b6255e64b2ece96fcba001a1a38
Farhan-Ali1786 <farhan12345ali12346@gmail.com>
2026-06-08 18:35:45 +0500

    changes

--- app/Http/Controllers/Api/BookkaruCancellationController.php
+++ app/Http/Controllers/Api/BookkaruCancellationController.php
@@ -118,8 +118,11 @@ class BookkaruCancellationController extends Controller
 
                 if ($request->filled('booking_reference')) {
                     $bookingReference = $this->normalizeBookingReference($request->booking_reference);
-                    $mismatchedBooking = $requestedTickets->contains(function ($ticket) use ($bookingReference) {
-                        return (string) $ticket->booking_no !== (string) $bookingReference;
+                    $rawBookingReference = trim((string) $request->booking_reference);
+                    $mismatchedBooking = $requestedTickets->contains(function ($ticket) use ($bookingReference, $rawBookingReference) {
+                        return (string) $ticket->booking_no !== (string) $bookingReference
+                            && (string) $ticket->transaction_id !== (string) $rawBookingReference
+                            && (string) $ticket->transaction_id !== (string) $bookingReference;
                     });
 
                     if ($mismatchedBooking) {
