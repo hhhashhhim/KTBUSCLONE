1f16f1ff75a771bbe3208f9bf0030f18f46bba67
Farhan-Ali1786 <farhan12345ali12346@gmail.com>
2026-06-13 20:20:19 +0500

    changes

--- app/Http/Controllers/Booking/BookingController.php
+++ app/Http/Controllers/Booking/BookingController.php
@@ -1177,7 +1177,7 @@ class BookingController extends Controller
             })
             ->first();
         $scheduleSurcharge = Surcharge::where('id', $schedule->surcharge_id)->where('is_active', 1)->first();
-        $fareForAllClasses = FareTable::where('from_city_id', $request->departureCity)->where('to_city_id', $request->destinationCity)
+       return $fareForAllClasses = FareTable::where('from_city_id', $request->departureCity)->where('to_city_id', $request->destinationCity)
             ->where('company_id', Auth::user()->company_id)
             ->get()->unique('fare_class');
         // getting cities sequence for checking which city will be after other one
