79cbf6e4d9d9407a0bf67351da5c766ff9e369e0
Farhan-Ali1786 <farhan12345ali12346@gmail.com>
2026-07-07 20:18:22 +0500

    changes

--- app/Http/Controllers/Schedule/ScheduleClosingController.php
+++ app/Http/Controllers/Schedule/ScheduleClosingController.php
@@ -160,21 +160,21 @@ class ScheduleClosingController extends Controller
             });
 
         // Always apply allowed route filter
-        if (!$user->is_super_admin) {
-            if (empty($finalRouteIds)) {
-                $query->whereRaw('1 = 0');
-            } else {
-                $query->whereHas('schedule', function ($q) use ($finalRouteIds) {
-                    $q->whereIn('route_id', $finalRouteIds);
-                });
-            }
-        } elseif (!empty($finalRouteIds)) {
-            // Super admin + dropdownRoute filter
-            $query->whereHas('schedule', function ($q) use ($finalRouteIds) {
-                $q->whereIn('route_id', $finalRouteIds);
-            });
-        }
-        return "ad";
+        // if (!$user->is_super_admin) {
+        //     if (empty($finalRouteIds)) {
+        //         $query->whereRaw('1 = 0');
+        //     } else {
+        //         $query->whereHas('schedule', function ($q) use ($finalRouteIds) {
+        //             $q->whereIn('route_id', $finalRouteIds);
+        //         });
+        //     }
+        // } elseif (!empty($finalRouteIds)) {
+        //     // Super admin + dropdownRoute filter
+        //     $query->whereHas('schedule', function ($q) use ($finalRouteIds) {
+        //         $q->whereIn('route_id', $finalRouteIds);
+        //     });
+        // }
+       
         $buses = Bus::where('company_id', $user->company_id)
             ->select('id', 'bus_number')
             ->orderBy('bus_number')
